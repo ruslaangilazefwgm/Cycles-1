a2fdf01fe976f880935402a39714b962eb908fc2
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-23 10:04:28 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -41,5 +41,24 @@ public class Main {
         for (int i = 1; i <= 512; i*=2) {
             System.out.println(i);
         }
+
+        // Задание 3.1
+        System.out.println("Задание 3.1");
+        int deposit = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + deposit;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        // Задание 3.2
+        System.out.println("Задание 3.2");
+        //int deposit = 29000;
+        //int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + total/100;
+            total = total + deposit;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
     }
 }
