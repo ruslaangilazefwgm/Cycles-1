120a76f8cfbef8e9e1690c0247265f194e3ff431
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-23 10:49:15 +0300

    task 3

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,7 @@ public class Main {
         }
 
         // Задание 3.2
-        System.out.println("Задание 3.2");
+        System.out.println("Задание 3.2 ");
         //int deposit = 29000;
         double total2 = 0;
         for (int i = 1; i <= 12; i++) {
