60518cd89dbda0dc82dbbec9874f00e8e5cedd58
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-23 09:43:47 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,23 @@ public class Main {
         for (int i =10; i >= -10; i--) {
             System.out.println(i);
         }
+
+        // Задание 2.1
+        System.out.println("Задание 2.1");
+        for (int i = 1904; i <= 2096; i+=4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        // Задание 2.2
+        System.out.println("Задание 2.2");
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.println(i);
+        }
+
+        // Задание 2.3
+        System.out.println("Задание 2.3");
+        for (int i = 1; i <= 512; i*=2) {
+            System.out.println(i);
+        }
     }
 }
