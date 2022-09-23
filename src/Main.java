e7810662793b230064fafb179be2d3a0432a3de0
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-23 09:30:17 +0300

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        System.out.println("Задание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        // Задание 2
+        System.out.println("Задание 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        // Задание 3
+        System.out.println("Задание 3");
+        for (int i = 0; i <= 17; i+=2) {
+            System.out.println(i);
+        }
+
+        // Задание 4
+        System.out.println("Задание 4");
+        for (int i =10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
